b78ebd11c15a8b568f92f69c15ffbe0446525284
TumurLagawy <timurtagirovich@mail.ru>
2022-04-01 21:01:53 +0300

    variable of scanner

--- src/com/metanit/Program.java
+++ src/com/metanit/Program.java
@@ -1,5 +1,7 @@
 package com.metanit;
 
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
         /*
@@ -10,12 +12,25 @@ public class Program {
         System.out.println ("Переменная var1 содержит:" + var1 ) ;
         var2 = var1 / 2 ;
         System.out.print ("Переменная var2 содержит:" +var2);
-         */
+         *//*
         String name = "Tom";
         int age = 31;
         float height = 1.7f;
 
         System.out.printf("Name: %s Age: %d Height: %.2f \n", name,age,height);
+        *//*
+        Scanner in=new Scanner(System.in);
+        System.out.println("Input a number: ");
+        int num=in.nextInt();
+
+        System.out.println("Your number is  " +num);
+        in.close();
+        */Scanner scanner=new Scanner(System.in);
+        System.out.println("Input a number: ");
+        int num=scanner.nextInt();
+
+        System.out.println("Your number is  " +num);
+        scanner.close();
     }
 
 }
