8c9a866de8914735b29957ebbe2c129eb12414c2
TumurLagawy <timurtagirovich@mail.ru>
2022-04-01 19:49:11 +0300

    Hello! Welcome to Java! With Poem2

---
+++ src/com/metanit/Poem.java
@@ -0,0 +1,19 @@
+package com.metanit;
+
+public class Poem {
+    public static void main(String[] args) {
+/*
+        String text = "  Вот мысль, которой весь я предан, \n " +
+                "Итог всего, что ум скопил. \n " +
+                "Лишь тот, кем бой за жизнь изведан, \n " +
+                "Жизнь и свободу заслужил.";
+        System.out.println(text);
+
+ */String text = """
+                Вот мысль, которой весь я предан,
+                Итог всего, что ум скопил.
+                Лишь тот, кем бой за жизнь изведан,
+                Жизнь и свободу заслужил.""";
+        System.out.println(text);
+    }
+}
---
+++ src/com/metanit/Program.java
@@ -0,0 +1,15 @@
+package com.metanit;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+        int var1 ;
+        int var2 ;
+        var1 = 1024 ;
+        System.out.println ("Переменная var1 содержит:" + var1 ) ;
+        var2 = var1 / 2 ;
+        System.out.print ("Переменная var2 содержит:" +var2);
+
+    }
+
+}
