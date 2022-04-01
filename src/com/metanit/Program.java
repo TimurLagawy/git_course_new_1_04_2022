0c76f9649c066a6c97e34fffae4d1bbda1c8865c
TumurLagawy <timurtagirovich@mail.ru>
2022-04-01 20:20:25 +0300

    info about Tom

--- src/com/metanit/Program.java
+++ src/com/metanit/Program.java
@@ -2,6 +2,7 @@ package com.metanit;
 
 public class Program {
     public static void main(String[] args) {
+        /*
         System.out.println("Hello Java!");
         int var1 ;
         int var2 ;
@@ -9,7 +10,12 @@ public class Program {
         System.out.println ("Переменная var1 содержит:" + var1 ) ;
         var2 = var1 / 2 ;
         System.out.print ("Переменная var2 содержит:" +var2);
+         */
+        String name = "Tom";
+        int age = 31;
+        float height = 1.7f;
 
+        System.out.printf("Name: %s Age: %d Height: %.2f \n", name,age,height);
     }
 
 }
